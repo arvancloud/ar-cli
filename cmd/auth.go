01b7ef31a6bc57b70291cefa45888102dddc8dc6
ebi <eahmadi641@gmail.com>
2021-01-12 22:21:22 +0330

    dock(auth-segment): update docs

--- cmd/auth.go
+++ cmd/auth.go
@@ -1,18 +1,3 @@
-/*
-Copyright © 2021 NAME HERE <EMAIL ADDRESS>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package cmd
 
 import (
@@ -21,16 +6,16 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// authCmd represents the auth command
+var (
+	authDesc = ` Log in to Arvan API and save login for subsequent use
+    First-time users of the client should run this command to connect to a Arvan API,
+    establish an authenticated session, and save connection to the configuration file.`
+)
+
 var authCmd = &cobra.Command{
 	Use:   "auth",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Log in to Arvan server",
+	Long:  authDesc,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("auth called")
 	},
