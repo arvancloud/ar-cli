a41c4e9fd4abb70eec003827927d461d86357d52
ebi <eahmadi641@gmail.com>
2021-01-14 01:14:31 +0330

    refactor(http): redisgn http utils

--- pkg/http/http.go
+++ pkg/http/http.go
@@ -3,48 +3,56 @@ package http
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"log"
 	"net/http"
 )
 
-var ApiKey string = "" // todo: read from config file
+var ApiKey string = "Apikey 9181cf76-7880-4834-b333-0dba8c4915c9" // todo: read from config file
 var ContentType = "application/json"
 
-var potentialSuccessStatusCodes = map[int]string{
-	201: "Resource created Successfully",
-	200: "ok",
-}
-
-var potentialErrorStatusCodes = map[int]string{
-	401: "Access token is missing or invalid",
-	404: "Resource not found",
-	422: "The given data was invalid",
-}
-
-func Post(url string, payload map[string]string) {
-	postBody, _ := json.Marshal(payload)
-	req, newRequestErr := http.NewRequest("POST", url, bytes.NewBuffer(postBody))
-
-	if newRequestErr != nil {
-		log.Fatal(newRequestErr)
-	}
+func Get(url string, routeParams map[string]string)(*http.Response, error) {
+	req := newRequest("GET", url, routeParams)
 
 	response, responseErr := do(req)
 
 	if responseErr != nil {
-		log.Fatal(responseErr)
+		return nil, responseErr
 	}else {
-		handleResponse(response)
+		return handleResponse(response)
 	}
 }
 
-func handleResponse(response *http.Response){
-	hasHttpError := potentialErrorStatusCodes[response.StatusCode] != ""
+func Post(url string, payload map[string]string) (*http.Response, error) {
+	req := newRequest("POST", url, payload)
 
-	if  hasHttpError{
-		log.Fatal(response.Status)
+	response, responseErr := do(req)
+
+	if responseErr != nil {
+		return nil, responseErr
 	}else {
-		println(potentialSuccessStatusCodes[response.StatusCode])
+		return handleResponse(response)
+	}
+}
+
+func newRequest(method string, url string, payload map[string]string) *http.Request {
+	postBody, _ := json.Marshal(payload)
+	req,err := http.NewRequest(method, url, bytes.NewBuffer(postBody))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return req
+}
+
+func handleResponse(response *http.Response) (*http.Response, error) {
+	statusOK := response.StatusCode >= 200 && response.StatusCode < 300
+
+	if !statusOK{
+		return nil, errors.New(response.Status)
+	}else {
+		return response, nil
 	}
 }
 
@@ -52,5 +60,11 @@ func do(req *http.Request) (*http.Response, error){
 	req.Header.Set("content-type", ContentType)
 	req.Header.Set("Authorization", ApiKey)
 
-	return http.DefaultClient.Do(req)
+	res, err := http.DefaultClient.Do(req)
+
+	if err != nil{
+		return nil, err
+	}
+
+	return res, nil
 }
