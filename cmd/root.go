14fb522cff1122f0d5c1daabc3f8de857f6ab9e1
ebi <eahmadi641@gmail.com>
2021-01-19 19:44:52 +0330

    feat(core): add descriptive description

--- cmd/root.go
+++ cmd/root.go
@@ -60,13 +60,7 @@ func newTable(tableHeaders []string) *tablewriter.Table {
 
 var rootCmd = &cobra.Command{
 	Use:   "ar-cli",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "This package provides a unified command line interface to Arvan CDN Services.",
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
