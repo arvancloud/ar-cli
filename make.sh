ac8804ace46ba336fd3d0cf13a438fd298a38dd2
Masih Yeganeh <goodboy.php@gmail.com>
2021-01-19 16:53:26 +0330

    Change Makefile to bash script

--- make.sh
+++ make.sh
@@ -1,4 +1,4 @@
-#@IgnoreInspection BashAddShebang
+#!/usr/bin/env bash
 export ROOT=$(realpath $(dir $(lastword $(MAKEFILE_LIST))))
 export CGO_ENABLED=0
 export GO111MODULE=on
@@ -6,5 +6,4 @@ export GOPROXY=https://proxy.golang.org,direct
 
 export ENV=development
 
-build:
-	go build -v -o $(ROOT)/bin/ar-cli $(ROOT)/cmd/ar-cli/*.go
+go build -v -o $(ROOT)/bin/ar-cli $(ROOT)/cmd/ar-cli/*.go
